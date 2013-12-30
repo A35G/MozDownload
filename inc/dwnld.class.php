fc3912255321402b34130477212bf31df3bd8e83
A35G <a35g@hackworld.it>
2013-12-30 22:23:52 +0100

    Beta version of script

--- inc/dwnld.class.php
+++ inc/dwnld.class.php
@@ -16,6 +16,7 @@
 
     var $version_info = vrs_list;
     var $dimn_info = dim_list;
+    var $url_downl = dwn_list;
     private $info = array();
     private $agent = "";
     private $buttond;
@@ -77,7 +78,7 @@
           CURLOPT_RETURNTRANSFER => 1,
           CURLOPT_URL => $url_svn,
           CURLOPT_USERAGENT => 'GMCode.it - Automatic Download Script - Beta 
-          version'
+          version - https://github.com/A35G/MozDownload'
       ));
 
       // Send the request & save response to $resp
@@ -110,11 +111,13 @@
 
           switch(htmlentities($type_url)) {
             case 'last_version':
-              // With 'TRUE', returned objects will be converted into associative arrays.
+              //  With 'TRUE', returned objects will be converted into
+              //  associative arrays.
               $arr_v = json_decode($this->version_info, true);
             break;
             case 'dim_files':
-              // With 'TRUE', returned objects will be converted into associative arrays.
+              //  With 'TRUE', returned objects will be converted into
+              //  associative arrays.
               $arr_v = json_decode($this->dimn_info, true);
             break;
           }
@@ -200,11 +203,30 @@
 
     protected function infoProgram($prgm) {
       $svn_file = $this->svnUrl($prgm, 'last_version');
-      if ($this->checkUrl($svn_file))
+      if ($this->checkUrl($svn_file)) {
         //return $svn_file;
-        return $this->getInfoVrs($svn_file);
-      else
+        //return $this->getInfoVrs($svn_file);
+        $this->data_svn = json_decode($this->getInfoVrs($svn_file), true);
+        return $this->data_svn;
+      } else {
         die($this->fix_text['NTURL05']);
+      }
+    }
+
+    protected function getLink($prgm) {
+      $bsl = "LATEST_" . strtoupper($prgm) . "_VERSION";
+      if (array_key_exists($bsl, $this->data_svn)) {
+        $prod_download = strtolower($prgm)."-".$this->data_svn[$bsl];
+        return sprintf($this->url_downl, $prod_download, $this->info['oper_sys'], $this->info['lang_user']);
+      }
+    }
+
+    protected function getActualName($prgm) {
+      $bsl = "LATEST_" . strtoupper($prgm) . "_VERSION";
+      if (array_key_exists($bsl, $this->data_svn)) {
+        $prod_download = ucfirst($prgm)." ".$this->data_svn[$bsl];
+        return $prod_download;
+      }
     }
 
     public function getVrs($prgname) {
@@ -220,7 +242,15 @@
     }
 
     public function dataProgram($prgm) {
-      return $this->infoProgram($prgm);
+      /*return */$this->infoProgram($prgm);
+    }
+
+    public function getDownload($prgm) {
+      return $this->getLink($prgm);
+    }
+
+    public function getNameAct($prgm) {
+      return $this->getActualName($prgm);
     }
 
     public function getButtDownload($ts, $tpl) {
--- index.php
+++ index.php
@@ -7,10 +7,13 @@
   //echo $mid->getUrlProgm('firefox', 'dim_files');
   //echo @$mid->errorAppl();
   //echo ($mid->validUrl('firefox', 'dim_files')) ? 'Valid!' : 'Not Valid!';
-  echo $mid->datailUsr();
+  //echo $mid->datailUsr();
+  /*echo */$mid->dataProgram('firefox');
   
   $ts = array(
     "dwln_prgm" => "firefox",
-    //"url_dwln" => $mid->getDownload('firefox');
-    //"name_prgm" => $mid->getNameAct('firefox');
+    "url_dwln" => $mid->getDownload('firefox'),
+    "name_prgm" => $mid->getNameAct('firefox')
   );
+
+  echo $mid->getButtDownload($ts, 'tpl/appl.html');
